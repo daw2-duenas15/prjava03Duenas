aedcc7c106f37425668961ac253e6c42ace4052a
ubu2 <ubu2@localhost>
2015-05-13 17:58:58 +0200

    Versió 0.4-B del projecte prjava03cognom. Branca00 creada i modificada.

--- src/prjava03/duenas15/Prjava03Duenas15.java
+++ src/prjava03/duenas15/Prjava03Duenas15.java
@@ -1,6 +1,7 @@
 package prjava03.duenas15; 
 import java.awt.*;        // using AWT containers and components 
 import java.awt.event.*;  // using AWT events and listener interfaces 
+import java.net.*;
  
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava03Duenas15 extends Frame 
